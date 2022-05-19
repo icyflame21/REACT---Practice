6b68c7b45960fbca47b4525b80bab5cb37580773
biswa-ranjan-subudhi <biswaranjan.cuh@gmail.com>
2022-05-19 16:52:21 +0530

    Initial Commit

--- src/Components/StopWatch/StopWatch.jsx
+++ src/Components/StopWatch/StopWatch.jsx
@@ -1,26 +1,43 @@
 import React, { useState, useEffect } from "react";
-
+import './StopWatch.css'
 export const StopWatch = () => {
   const [timerOn, setTimeron] = useState(false);
   const [time, setTime] = useState(0);
 
   useEffect(() => {
-    let interval=null
+    let interval = null;
     if (timerOn) {
-        
-      }
-    },[time])
+      interval = setInterval(() => {
+        setTime((prev) => prev + 10);
+      }, 10);
+    } else if (!timerOn) {
+      clearInterval(interval);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [timerOn]);
 
   return (
     <>
       <div className="Timer">
         <h2>StopWatch</h2>
-        <div id="display">{ time}</div>
+        <div id="display">
+          <span>{("0"+Math.floor((time/60000)%60)).slice(-2)}:</span>
+          <span>{("0"+Math.floor((time/1000)%60)).slice(-2) }:</span>
+          <span>{("0"+((time/10)%100)).slice(-2)}</span>
+        </div>
         <div id="buttons">
-          <button onClick={()=>setTimeron(true)}>START</button>
-          <button onClick={() => setTimeron(false)}>STOP</button>
-          <button onClick={() => setTimeron(true)}>RESUME</button>
-          <button onClick={() => setTime(0)}>RESET</button>
+          {!timerOn && time === 0 && (
+            <button onClick={() => setTimeron(true)}>START</button>
+          )}
+          {timerOn && <button onClick={() => setTimeron(false)}>STOP</button>}
+          {!timerOn && time > 0 && (
+            <button onClick={() => setTimeron(true)}>RESUME</button>
+          )}
+          {!timerOn && time > 0 && (
+            <button onClick={() => setTime(0)}>RESET</button>
+          )}
         </div>
       </div>
     </>
