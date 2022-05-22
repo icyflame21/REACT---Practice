0b23d8af76a0800ef5234418730d67913fdeadc5
biswa-ranjan-subudhi <biswaranjan.cuh@gmail.com>
2022-05-23 02:19:31 +0530

    .

--- src/Components/PolyFills-Filter,reduce,map.js
+++ src/Components/PolyFills-Filter,reduce,map.js
@@ -1,11 +1,11 @@
-map(), filter(),reduce() methods are invoked on arrays and written in Array class in JavaScript
+// map(), filter(),reduce() methods are invoked on arrays and written in Array class in JavaScript
 
- <!-- map() method -->
+//  <!-- map() method -->
 
 var a=[5,2,6,8];
 var r = a.map(item => item*2);          // map() is invoked here
 console.log(r);
-The output is: Array [10, 4, 12, 16]
+// The output is: Array [10, 4, 12, 16]
 
 Syntax:- 
 let new_array = arr.map(function callback( currentValue, index, array) {
@@ -13,17 +13,17 @@ let new_array = arr.map(function callback( currentValue, index, array) {
 },[thisArg])
 
 
-// Custom map
-// Array.prototype.myMap = function (callback) {
-//     let arr = []
-//     for (var i = 0; i < this.length; i++) {
-//     arr.push(callback(this[i]))
-//     }
-//     return arr
-// }
-// var a = [5, 2, 6, 8]
-// var r = a.myMap(e => e * 3)
-// console.log(r);
+Custom map
+Array.prototype.myMap = function (callback) {
+    let arr = []
+    for (var i = 0; i < this.length; i++) {
+    arr.push(callback(this[i]))
+    }
+    return arr
+}
+var a = [5, 2, 6, 8]
+var r = a.myMap(e => e * 3)
+console.log(r);
 
 Syntax:- let newArray = arr.filter(callback(element, index,array)[thisArg])
 
